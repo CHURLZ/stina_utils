a5478527604fefd51138353b751731fcaaf0fd5e
CHURLZ <kalle.berglund@gmail.com>
2017-11-08 17:22:19 +0100

    new attempt at setup.py

--- setup.py
+++ setup.py
@@ -1,26 +1,11 @@
-import uuid
-from setuptools import setup
-import os
-from pip.req import parse_requirements
+from setuptools import find_packages, setup
 
-def read(fname):
-    return open(os.path.join(os.path.dirname(__file__), fname)).read()
-
-BASE_DIR = os.path.dirname(os.path.realpath(__file__))
-reqs_file = os.path.join(BASE_DIR, 'requirements.txt')
-install_reqs = parse_requirements(reqs_file, session=uuid.uuid1())
-
-setup(
-    name="stina_utils",
-    version="0.1.1",
-    author="Stina Labs @ Stena IT",
-    author_email="carlos@stena.io",
-    description=("Support utils for Stina project"),
-    license="MIT",
-    keywords="utils",
-    install_requires=["Flask"],
-    url="https://github.com/StenaTransformations/stina_utils",
-    packages=[],
-    include_package_data=False,
-    long_description=read('README.md')
-)
+setup(name='stina_utils',
+      version='0.1.0',
+      description='Support utils for Stina',
+      author='Ludvig Gee, Carl Berglund',
+      author_email='ludvig.gee@stena.io',
+      url='https://github.com/StenaTransformations/stina_utils',
+      license='MIT',
+      install_requires=['Flask'],
+      packages=find_packages())
